59655c4a799f236bc3f1b24173e810fde58e92a3
mudegiarnold <mudegi4@gmail.com>
2024-04-20 12:21:54 +0300

    Outputting Variables

---
+++ output.py
@@ -0,0 +1,21 @@
+x = "Python is awesome"
+print(x)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y ,z)
+
+x = "python"
+y = "is"
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "john"
+print(x, y)
+
