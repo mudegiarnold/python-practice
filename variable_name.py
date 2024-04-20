4984210e556a7475686c589d30e7b3b5e2b552a7
mudegiarnold <mudegi4@gmail.com>
2024-04-20 11:37:38 +0300

    Added variable_name.py

---
+++ variable_name.py
@@ -0,0 +1,13 @@
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
