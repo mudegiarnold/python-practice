743efe7218ac4be884474309b580de5fae840213
mudegiarnold <mudegi4@gmail.com>
2024-04-20 16:47:52 +0300

    Added Global Variables

---
+++ global.py
@@ -0,0 +1,20 @@
+x = "awesome"
+
+def myfunc():
+    x = "fantastic"
+    print("Python is " + x)
+
+    myfunc()
+
+    print("Python is " + x)
+
+
+
+def myfunc():
+    global x
+    x = "fantastic"
+
+    myfunc()
+
+    print("Python is " + x)
+
