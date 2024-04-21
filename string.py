5351f60b9c45c75462f662abe58d74f9f001796d
mudegiarnold <mudegi4@gmail.com>
2024-04-21 09:26:12 +0300

    Added content

---
+++ string.py
@@ -0,0 +1,38 @@
+print("MudegiArnold")
+print('mudegiarnold')
+
+print("It's alright")
+print("He is called 'John'")
+print('He is called "Johnny"')
+
+a = "Hello"
+print(a)
+
+a = """My name is mudegi arnold and i love coding during night hours."""
+print(a)
+
+a = '''My mother is Masibo Juliet and i love her so much.'''
+print(a)
+
+a = "Hello, World!"
+print(a[1])
+
+for x in "banana":
+    print(x)
+
+a = "Hello, World!"
+print(len(a))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+    print("Yes, 'free' is present.")
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+    
+
+
