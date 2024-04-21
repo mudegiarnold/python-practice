665e4d35e2f4ded9fc995b746914dcfe262723d4
mudegiarnold <mudegi4@gmail.com>
2024-04-21 08:50:08 +0300

    Specifying a Variable Type

---
+++ casting.py
@@ -0,0 +1,23 @@
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4")
+print(x)
+print(y)
+print(z)
+print(w)
+
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+
