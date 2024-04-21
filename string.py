9773b9b2fff327225c825f7767a7c38ec8a33cb4
mudegiarnold <mudegi4@gmail.com>
2024-04-21 09:32:18 +0300

    Using it in an if statement

--- string.py
+++ string.py
@@ -33,6 +33,8 @@ if "free" in txt:
 txt = "The best things in life are free!"
 print("expensive" not in txt)
 
-    
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+    print("No, 'expensive' is NOT present.")
 
 
