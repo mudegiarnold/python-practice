358bb8d3ade1ef5820403dbc08a5597ab3c6842f
mudegiarnold <mudegi4@gmail.com>
2024-04-22 16:41:35 +0300

    Changing the second value by replacing it with two new values

--- change.py
+++ change.py
@@ -6,3 +6,7 @@ thislist = ["apple", "banana", "cherry", "cherry", "orange", "kiwi", "mango"]
 thislist[1:3] = ["blackcurrent", "watermelon"]
 print(thislist)
 
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrent", "watermelon"]
+print(thislist)
+
