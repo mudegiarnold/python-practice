33fa546c4569b9dfa43aae69f516484daedb332b
mudegiarnold <mudegi4@gmail.com>
2024-04-22 16:14:53 +0300

    Specifying range of indexes

--- access.py
+++ access.py
@@ -4,3 +4,6 @@ print(thislist[1])
 thislist = ["apple", "banana", "cherry"]
 print(thislist[-1])
 
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
