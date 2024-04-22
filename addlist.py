bad81d6fa0f17481bcbc9a015f66711d9c964d96
mudegiarnold <mudegi4@gmail.com>
2024-04-22 17:01:58 +0300

    Adding elements of a tuple to a list

--- addlist.py
+++ addlist.py
@@ -11,3 +11,8 @@ tropical = ["mango", "pineapple", "papaya"]
 thislist.extend(tropical)
 print(thislist)
 
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+
