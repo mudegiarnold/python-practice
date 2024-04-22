d39db4b6df846990deb1ae8aaff28907ef20f6d4
mudegiarnold <mudegi4@gmail.com>
2024-04-22 16:19:00 +0300

    Returning items from the beginning to, but NOT including, 'kiwi'

--- access.py
+++ access.py
@@ -7,3 +7,7 @@ print(thislist[-1])
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[2:5])
 
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+
+
