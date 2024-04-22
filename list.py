d34b7658ae9fcf2a585108221c71c7ddcad1937d
mudegiarnold <mudegi4@gmail.com>
2024-04-22 15:48:56 +0300

    list items can be any data types

--- list.py
+++ list.py
@@ -7,3 +7,10 @@ print(thislist)
 thislist = ["apple", "banana", "cherry"]
 print(len(thislist))
 
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+print(list1)
+print(list2)
+print(list3)
+
