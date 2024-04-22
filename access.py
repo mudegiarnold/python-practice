7a0425048ddf5052170d32a728ec897723c28a9f
mudegiarnold <mudegi4@gmail.com>
2024-04-22 16:26:09 +0300

    Returning items from 'orange' to, but NOT including 'mango'

--- access.py
+++ access.py
@@ -13,4 +13,5 @@ print(thislist[:4])
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[2:])
 
-
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
