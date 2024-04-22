ee4d8da912f11ee56e946d951f49a98484cda85a
mudegiarnold <mudegi4@gmail.com>
2024-04-22 16:37:47 +0300

    Changing values

--- change.py
+++ change.py
@@ -2,3 +2,7 @@ thislist = ["apple", "banana", "cherry"]
 thislist[1] = "blackcurrent"
 print(thislist)
 
+thislist = ["apple", "banana", "cherry", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrent", "watermelon"]
+print(thislist)
+
