ce7dff85e1f9f90f345522a9f640ba39b97e6271
mudegiarnold <mudegi4@gmail.com>
2024-04-22 16:58:03 +0300

    Using the extend() method to append elements

--- addlist.py
+++ addlist.py
@@ -6,3 +6,8 @@ thislist = ["apple", "banana", "cherry"]
 thislist.insert(1, "orange")
 print(thislist)
 
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
