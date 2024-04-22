1eb9124be63f4de46889b13b509c801a11da8e33
mudegiarnold <mudegi4@gmail.com>
2024-04-22 16:22:27 +0300

    Returning items from 'cherry' to the end

--- access.py
+++ access.py
@@ -10,4 +10,7 @@ print(thislist[2:5])
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[:4])
 
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+
 
