ad6923822f95fae0d4fb767271c4c1db6f8898c1
mudegiarnold <mudegi4@gmail.com>
2024-04-22 16:47:29 +0300

    Inserting 'watermelon' as the third item

--- change.py
+++ change.py
@@ -14,3 +14,7 @@ thislist = ["apple", "banana", "cherry"]
 thislist[1:3] = ["watermelon"]
 print(thislist)
 
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
