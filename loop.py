e150bea0cddafbd865d55d5a9042b671e30351ec
mudegiarnold <mudegi4@gmail.com>
2024-04-23 15:46:36 +0300

    Using a while loop

--- loop.py
+++ loop.py
@@ -6,3 +6,8 @@ thislist = ["apple", "banana", "cherry"]
 for i in range(len(thislist)):
     print(thislist[i])
 
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+    print(thislist[i])
+    i = i + 1
