986eb0e6d3feb4d179722554b093b0bae7a53358
mudegiarnold <mudegi4@gmail.com>
2024-04-24 16:37:48 +0300

    Third example

--- item.py
+++ item.py
@@ -12,3 +12,8 @@ print(thistuple[2:5])
 
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(thistuple[:4])
+
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+
