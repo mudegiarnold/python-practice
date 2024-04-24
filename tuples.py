b89a155bfdfec4d6a073861ae8bbae96a219a4d3
mudegiarnold <mudegi4@gmail.com>
2024-04-24 15:59:34 +0300

    Creating Tuple With one item

--- tuples.py
+++ tuples.py
@@ -9,3 +9,11 @@ print(thistuple)
 thistuple = ("apple", "banana", "cherry")
 print(len(thistuple))
 
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
