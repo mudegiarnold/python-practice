927dcd9b9ba28f7cf0f80118b9a321583c04e7cb
mudegiarnold <mudegi4@gmail.com>
2024-04-24 14:55:36 +0300

    Reversing order of the list items

--- sort.py
+++ sort.py
@@ -31,3 +31,8 @@ thislist = ["banana", "Orange", "Kiwi", "cherry"]
 thislist.sort(key = str.lower)
 print(thislist)
 
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
+
