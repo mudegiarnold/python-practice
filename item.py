c074cbc7067c94585d420d26537948739cca60c3
mudegiarnold <mudegi4@gmail.com>
2024-04-24 16:41:08 +0300

    Range of Negative Indexes

--- item.py
+++ item.py
@@ -17,3 +17,7 @@ print(thistuple[:4])
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(thistuple[2:])
 
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
