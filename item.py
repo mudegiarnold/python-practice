891b2bbb431199c57abeb0166c6219ab9af0a703
mudegiarnold <mudegi4@gmail.com>
2024-04-24 16:30:36 +0300

    Returning the third, fourth, and fifth item

--- item.py
+++ item.py
@@ -5,3 +5,7 @@ print(thistuple[1])
 thistuple = ("apple", "banana", "cherry")
 print(thistuple[-1])
 
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
