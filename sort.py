06d16b655de7c39fe50c84299054f7acb17936d5
mudegiarnold <mudegi4@gmail.com>
2024-04-24 14:47:01 +0300

    Customizing a function using 'key = function'@

--- sort.py
+++ sort.py
@@ -14,3 +14,10 @@ thislist = [100, 50, 65, 82, 23]
 thislist.sort(reverse = True)
 print(thislist)
 
+def myfunc(n):
+    return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+
