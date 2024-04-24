20d3d37d333568f9138fdc2738f973996ca4c3ff
mudegiarnold <mudegi4@gmail.com>
2024-04-24 16:44:17 +0300

    Checking if Item Exists

--- item.py
+++ item.py
@@ -21,3 +21,9 @@ print(thistuple[2:])
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(thistuple[-4:-1])
 
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+    print("Yes, 'apple' is in the fruits tuple")
+
+
