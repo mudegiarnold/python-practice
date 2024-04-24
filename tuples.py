b0dfe8cb8250e83f805debb7fbcdaefa9d6aa722
mudegiarnold <mudegi4@gmail.com>
2024-04-24 16:07:53 +0300

    Tuple Items can be of any data type

--- tuples.py
+++ tuples.py
@@ -17,3 +17,11 @@ print(type(thistuple))
 thistuple = ("apple")
 print(type(thistuple))
 
+
+tuple1 = ("apple", "banana", "cherry",)
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+print(tuple1)
+print(tuple2)
+print(tuple3)
+
