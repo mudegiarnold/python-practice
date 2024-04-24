e86c5dea936f9c229ba052a6f7ba5b68e6230c70
mudegiarnold <mudegi4@gmail.com>
2024-04-24 14:32:05 +0300

    Returning 'orange' instead of 'banana'

--- comprehension.py
+++ comprehension.py
@@ -38,4 +38,9 @@ newlist = [x.upper() for x in fruits]
 
 print(newlist)
 
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x if x != "banana" else "orange" for x in fruits]
+
+print(newlist)
 
