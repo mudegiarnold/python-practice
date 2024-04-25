2e0792b2c723eb72b3a06cd3812446c0cd9f186c
mudegiarnold <mudegi4@gmail.com>
2024-04-25 08:32:11 +0300

    Looping Through the Index Numbers

--- looptuple.py
+++ looptuple.py
@@ -1,3 +1,8 @@
 thistuple = ("appple", "banana", "cherry")
 for x in thistuple:
     print(x)
+
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+    print(thistuple[i])
+
