9f01c15a2fbbdf07038ce60611a6d995c54b3320
mudegiarnold <mudegi4@gmail.com>
2024-04-25 11:00:49 +0300

    Keep items no present in both sets

--- joinset.py
+++ joinset.py
@@ -83,3 +83,9 @@ set1.difference_update(set2)
 print(set1)
 
 
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3 = set1.symmetric_difference(set2)
+print(set3)
+
+
