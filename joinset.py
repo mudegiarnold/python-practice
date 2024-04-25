b4ac06ab5e2d48ed56366fe8b700058b9df91ac9
mudegiarnold <mudegi4@gmail.com>
2024-04-25 10:42:06 +0300

    Joining set1 and set2, but keeping only the duplictes

--- joinset.py
+++ joinset.py
@@ -40,3 +40,10 @@ set2 = {1, 2, 3}
 set1.update(set2)
 print(set1)
 
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3 = set1.intersection(set2)
+print(set3)
+
+
