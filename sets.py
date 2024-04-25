40c634f684f4f99b68706e1d9de0a89496cbc36a
mudegiarnold <mudegi4@gmail.com>
2024-04-25 09:18:18 +0300

    String, int and boolean data types

--- sets.py
+++ sets.py
@@ -16,3 +16,11 @@ print(thisset)
 thisset = {"apple", "banana", "cherry"}
 print(len(thisset))
 
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+print(set1)
+print(set2)
+print(set3)
+
