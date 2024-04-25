bf4e9a3975781726757fbbb360e589f2187b29e2
mudegiarnold <mudegi4@gmail.com>
2024-04-25 08:17:31 +0300

    Using Asterisk

--- unpack.py
+++ unpack.py
@@ -5,3 +5,11 @@ fruits = ("apple", "banana", "cherry")
 print(green)
 print(yellow)
 print(red)
+
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
