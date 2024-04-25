c48bdf10bb31ba6c6bb3fef2a8d5bd21e3e8fb90
mudegiarnold <mudegi4@gmail.com>
2024-04-25 10:50:31 +0300

    Example 3

--- joinset.py
+++ joinset.py
@@ -59,3 +59,9 @@ set1.intersection_update(set2)
 print(set1)
 
 
+set1 = {"apple", 1, "banana", 0, "cherry"}
+set2 = {False, "google", 1, "apple", 2, True}
+set3 = set1.intersection(set2)
+print(set3)
+
+
