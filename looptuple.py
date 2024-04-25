a9816a330068869c12dabb0dd63a0e9466859749
mudegiarnold <mudegi4@gmail.com>
2024-04-25 08:35:59 +0300

    Using a While Loop

--- looptuple.py
+++ looptuple.py
@@ -6,3 +6,9 @@ thistuple = ("apple", "banana", "cherry")
 for i in range(len(thistuple)):
     print(thistuple[i])
 
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+    print(thistuple[i])
+    i = i + 1
+
