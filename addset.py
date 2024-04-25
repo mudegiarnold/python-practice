99fcd7d0c5e4622e4d198fd4fb3f24de0a773d19
mudegiarnold <mudegi4@gmail.com>
2024-04-25 09:57:50 +0300

    Adding elements of a list to at set

--- addset.py
+++ addset.py
@@ -6,3 +6,9 @@ thisset = {"apple", "banana", "cherry"}
 tropical = {"pineapple", "mango", "papaya"}
 thisset.update(tropical)
 print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+thisset.update(mylist)
+print(thisset)
+
