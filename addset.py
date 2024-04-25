d3ea88e5c7b358216645e2d13926bb4c72807fcd
mudegiarnold <mudegi4@gmail.com>
2024-04-25 09:54:36 +0300

    Adding elements from 'tropical' into 'thisset'

--- addset.py
+++ addset.py
@@ -2,3 +2,7 @@ thisset = {"apple", "banana", "cherry"}
 thisset.add("orange")
 print(thisset)
 
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+thisset.update(tropical)
+print(thisset)
