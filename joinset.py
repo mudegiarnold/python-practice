b42a0b021afd63e89031a5bc0b3e3df6cf767638
mudegiarnold <mudegi4@gmail.com>
2024-04-25 10:34:41 +0300

    Using | to join two sets

--- joinset.py
+++ joinset.py
@@ -19,3 +19,11 @@ set4 = {"apple", "banana", "cherry"}
 myset = set1.union(set2, set3, set4)
 print(myset)
 
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
+myset = set1 | set2 | set3 | set4 
+print(myset)
+
