2eda0147064ef4f4b6a39e82f67daa8b18d86f46
mudegiarnold <mudegi4@gmail.com>
2024-04-26 10:06:55 +0300

    Using del keyword to delete the dictionary completely

--- dictremove.py
+++ dictremove.py
@@ -21,3 +21,10 @@ thisdict = {"brand": "Ford",
 del thisdict["model"]
 print(thisdict)
 
+thisdict = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+        }
+del thisdict 
+print(thisdict)
