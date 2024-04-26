ff6d1e688a845581ce8d3ef769462966ccbe2aed
mudegiarnold <mudegi4@gmail.com>
2024-04-26 10:03:55 +0300

    using 'del' to remove an item

--- dictremove.py
+++ dictremove.py
@@ -14,3 +14,10 @@ thisdict = {
 thisdict.popitem()
 print(thisdict)
 
+thisdict = {"brand": "Ford",
+            "model": "Mustang",
+            "year": 1964
+            }
+del thisdict["model"]
+print(thisdict)
+
