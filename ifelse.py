55e76f535342bf04b689ee772edc5add8c3fac3f
mudegiarnold <mudegi4@gmail.com>
2024-04-26 11:38:37 +0300

    Using Esle

--- ifelse.py
+++ ifelse.py
@@ -10,3 +10,12 @@ if b > a:
 elif a == b:
     print("a and b are equal")
 
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater thsn b")
+
