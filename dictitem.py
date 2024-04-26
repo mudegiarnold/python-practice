f8979aab247fbd23d97ab0275ebc2c33c7307860
mudegiarnold <mudegi4@gmail.com>
2024-04-26 08:39:29 +0300

    Example 3

--- dictitem.py
+++ dictitem.py
@@ -66,3 +66,13 @@ print(x) #before the change
 car["year"] = 2020
 print(x) #after the change
 
+car = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+        }
+x = car.items()
+print(x) #before the change
+car["color"] = "red"
+print(x) #after the change
+
