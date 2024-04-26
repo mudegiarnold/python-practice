f797d3812d0542b253e4a4de5be06920bcc47379
mudegiarnold <mudegi4@gmail.com>
2024-04-26 09:54:59 +0300

    Using update() to add a color item

--- dictadd.py
+++ dictadd.py
@@ -6,3 +6,11 @@ thisdict = {
 thisdict["color"] = "red"
 print(thisdict)
 
+thisdict = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+        }
+thisdict.update({"color": "red"})
+print(thisdict)
+
