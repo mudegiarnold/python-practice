087784024c6c1eef57d95df3ba275c7252b91acd
mudegiarnold <mudegi4@gmail.com>
2024-04-26 08:25:50 +0300

    Updating the values list by making a change in the original dictionary

--- dictitem.py
+++ dictitem.py
@@ -28,3 +28,13 @@ x = thisdict.values()
 print(x)
 
 
+car = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+        }
+
+x = car.values()
+print(x) #before the change
+car["year"] = 2020
+print(x) #after the change
