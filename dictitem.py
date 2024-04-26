dcf76f3db0fc6c01dc16e0db01e51a8f32cd4be8
mudegiarnold <mudegi4@gmail.com>
2024-04-26 08:36:28 +0300

    Example 2

--- dictitem.py
+++ dictitem.py
@@ -55,4 +55,14 @@ print(x) #after the change
 x = thisdict.items()
 print(x)
 
+car = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+        }
+
+x = car.items()
+print(x) #before the change
+car["year"] = 2020
+print(x) #after the change
 
