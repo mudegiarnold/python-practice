a21f5f1bcfbc8d59a7b1c9a55cb8c3cb3fa92def
mudegiarnold <mudegi4@gmail.com>
2024-04-26 09:48:43 +0300

    Using update() method

--- dictchange.py
+++ dictchange.py
@@ -6,3 +6,11 @@ thisdict = {
 thisdict["year"] = 2018
 print(thisdict)
 
+thisdict = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+        }
+thisdict.update({"year": 2020})
+print(thisdict)
+
