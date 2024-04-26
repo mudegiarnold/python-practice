24f59603376fa49e9d954c3c3c8b1f51333a0dc8
mudegiarnold <mudegi4@gmail.com>
2024-04-26 07:35:56 +0300

    Duplicate values will overwrite existing values

--- dictionary.py
+++ dictionary.py
@@ -12,3 +12,11 @@ thisdict = {
         }
 print(thisdict["brand"])
 
+thisdict = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964,
+        "year": 2020
+        }
+print(thisdict)
+
