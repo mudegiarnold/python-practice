11f8b2c786d9ff652c127f1532cc9adf5afaabe0
mudegiarnold <mudegi4@gmail.com>
2024-04-26 08:29:45 +0300

    Adding a new item to the original dictionary

--- dictitem.py
+++ dictitem.py
@@ -38,3 +38,15 @@ x = car.values()
 print(x) #before the change
 car["year"] = 2020
 print(x) #after the change
+
+
+car = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+        }
+
+x = car.values()
+print(x) #before the change
+car["color"] = "red"
+print(x) #after the change
