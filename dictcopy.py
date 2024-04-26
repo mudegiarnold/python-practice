70a1c245fe2983c92f572b749144a9168bb7577d
mudegiarnold <mudegi4@gmail.com>
2024-04-26 10:31:44 +0300

    Using dict() to make a copy of adictionary

--- dictcopy.py
+++ dictcopy.py
@@ -6,3 +6,11 @@ thisdict = {
 mydict = thisdict.copy()
 print(mydict)
 
+thisdict = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+        }
+mydict = dict(thisdict)
+print(mydict)
+
