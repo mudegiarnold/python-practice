7acca0f874d667c6da479c7d3359dd7b5be43ee4
mudegiarnold <mudegi4@gmail.com>
2024-04-27 16:03:37 +0300

    Return Values

--- function.py
+++ function.py
@@ -48,5 +48,11 @@ def my_function(food):
 fruits = ["apple", "banana", "cherry"]
 
 my_function(fruits)
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
 
 
