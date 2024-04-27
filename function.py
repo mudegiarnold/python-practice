1062063b0ea41653ef37dd9509980f2e626b3651
mudegiarnold <mudegi4@gmail.com>
2024-04-27 15:54:12 +0300

    Keyword Arguments

--- function.py
+++ function.py
@@ -21,3 +21,9 @@ def my_function(fname, lname):
 def my_function(*kids):
                 print("The youngest child is " + kids[2])
                 my_function("Emil", "Tobias", "Linus")
+
+def my_function(child3, child2, child1):
+    print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
