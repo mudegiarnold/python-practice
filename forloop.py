7364c08e26ef5b6413601331aba8652aa3ddcdcf
mudegiarnold <mudegi4@gmail.com>
2024-04-27 15:04:30 +0300

    example 2

--- forloop.py
+++ forloop.py
@@ -10,3 +10,10 @@ for x in fruits:
     print(x)
     if x == "banana":
         break
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)
+
