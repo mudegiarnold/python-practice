f9c9ce67f3429cffcc6f18d2e1f559ec39bf50f3
mudegiarnold <mudegi4@gmail.com>
2024-04-27 15:49:01 +0300

    Example

--- function.py
+++ function.py
@@ -17,3 +17,7 @@ def my_function(fname, lname):
 
     my_function("Emil", "Refsnes")
 
+
+def my_function(*kids):
+                print("The youngest child is " + kids[2])
+                my_function("Emil", "Tobias", "Linus")
