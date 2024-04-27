0ed52741ac9b5e99e029a2f3480e86147be0141a
mudegiarnold <mudegi4@gmail.com>
2024-04-27 15:58:47 +0300

    Default Parameter Value

--- function.py
+++ function.py
@@ -32,4 +32,12 @@ def my_function(**kid):
   print("His last name is " + kid["lname"])
 
 my_function(fname = "Tobias", lname = "Refsnes")
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
 
