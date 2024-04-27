2c030a44c8710bf196dca26e23f12411688205a1
mudegiarnold <mudegi4@gmail.com>
2024-04-27 15:38:44 +0300

    Arguments

--- function.py
+++ function.py
@@ -2,3 +2,12 @@ def my_function():
     print("Hello from a function")
 
 my_function()
+
+
+def my_function(fname):
+    print(fname + "Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
